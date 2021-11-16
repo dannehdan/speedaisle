050653846537e1caa2d472a0934f97c393c841cd
Dan Marshall <daniel.o.marshall@gmail.com>
2021-11-16 11:51:59 +0000

    Restored branch to ignore coverage files

--- routes/list.js
+++ routes/list.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET list listing. */
 router.get('/', function(req, res, next) {
-  res.render('list/index', { title: 'SpeedAisle' });
+  res.render('list/index', { title: 'Your List' });
 });
 
 module.exports = router;
