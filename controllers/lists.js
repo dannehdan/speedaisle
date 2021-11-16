063ff8090a29596ec5af10b83443812f059c5fe9
frontendowl <frontendowl@gmail.com>
2021-11-16 15:51:38 +0000

    Add form for adding item

--- controllers/lists.js
+++ controllers/lists.js
@@ -1,8 +1,20 @@
 // require('../models/list.js');
+const categories = [
+  'Fruits & Vegs',
+  'Bakery',
+  'Fridge',
+  'Frozen',
+  'Food Cupboard',
+  'Household',
+];
 
 const ListsController = {
   Show: (req, res) => {
     res.render('list/index', { title: 'Your List' });
+  },
+
+  Add: (req, res) => {
+    res.send(`You added ${req.body.item} to the ${categories[req.body.category]} category`);
   }
 };
 
