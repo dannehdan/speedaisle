9ced6ccb1638ab92d717a588824ae62422142bed
Alex Nakov <alex.nakov3@gmail.com>
2021-11-18 11:37:29 +0000

    google auth routes work as expected

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,62 @@
+const { response } = require('express');
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+const cookieParser = require('cookie-parser')
+const {OAuth2Client} = require('google-auth-library');
+const CLIENT_ID = '588116689693-af8duu5dpqicmdai7ovfrcr5ii9bl4qb.apps.googleusercontent.com'
+const client = new OAuth2Client(CLIENT_ID);
+
+function checkAuthenticated(req, res, next){
+  let token = req.cookies['session-token'];
+
+  let user = {};
+  async function verify() {
+      const ticket = await client.verifyIdToken({
+          idToken: token,
+          audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
+      });
+      const payload = ticket.getPayload();
+      user.name = payload.name;
+      user.email = payload.email;
+      user.picture = payload.picture;
+    }
+    verify()
+    .then(()=>{
+        req.user = user;
+        next();
+    })
+    .catch(err=>{
+        res.redirect('/')
+    })
+}
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
+
+router.post('/', (req, res, next) => {
+  let token = req.body.token;
+
+  async function verify() {
+    const ticket = await client.verifyIdToken({
+        idToken: token,
+        audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
+    });
+    const payload = ticket.getPayload();
+    const userid = payload['sub'];
+    }
+    verify()
+    .then(()=>{
+        res.cookie('session-token', token);
+        res.send('success')
+    })
+  .catch(console.error);
+})
+
+router.get('/logout', checkAuthenticated, (req, res) => {
+  res.clearCookie('session-token')
+  res.redirect('/')
+})
 
 module.exports = router;
