ad48f2e21723fe7d53ccaea79b77df932e3cd6dc
Dan Marshall <daniel.o.marshall@gmail.com>
2021-11-18 10:37:17 +0000

    Cypress version to 8.7 and added uncaught:exception to support file

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -18,3 +18,11 @@
 
 // Alternatively you can use CommonJS syntax:
 require('./commands');
+
+
+Cypress.on('uncaught:exception', (err, runnable) => {
+  // returning false here prevents Cypress from
+  // failing the test
+  console.log(err);
+  return false;
+})
