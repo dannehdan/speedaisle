a60745f9a08b7dd2162b5893f6c23235d75730aa
parsa <parsamesg@gmail.com>
2021-11-19 16:48:27 +0000

    remove unneeded code

--- routes/location.js
+++ routes/location.js
@@ -1,39 +1,8 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-// const ListsController = require('../controllers/location');
-
-// const {OAuth2Client} = require('google-auth-library');
-// const CLIENT_ID = '588116689693-af8duu5dpqicmdai7ovfrcr5ii9bl4qb.apps.googleusercontent.com'
-// const client = new OAuth2Client(CLIENT_ID);
-
-// function checkAuthenticated(req, res, next){
-//   let token = req.cookies['session-token'];
-
-//   let user = {};
-//   async function verify() {
-//       const ticket = await client.verifyIdToken({
-//           idToken: token,
-//           audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
-//       });
-//       const payload = ticket.getPayload();
-//       user.name = payload.name;
-//       user.email = payload.email;
-//       user.picture = payload.picture;
-//     }
-//     verify()
-//     .then(()=>{
-//         req.user = user;
-//         next();
-//     })
-//     .catch(err=>{
-//         res.redirect('/')
-//     })
-// }
-
-// router.get('/', checkAuthenticated, ListsController.Show);
-router.get('/', function(req, res, next) {
-  res.render('location/index');
+router.get("/", function (req, res, next) {
+  res.render("location/index");
 });
 
-module.exports = router
+module.exports = router;
