c58a6f67c9536fa5b97117fd9256172f253511f6
Dan Marshall <daniel.o.marshall@gmail.com>
2021-11-19 11:46:48 +0000

    New location page

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var listRouter = require('./routes/list');
+var locationRouter = require('./routes/location');
 
 var app = express();
 
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/list', listRouter);
+app.use('/location', locationRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
