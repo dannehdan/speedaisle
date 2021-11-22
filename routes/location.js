e07a92d3806912c80ff80263e7c5386482b6f048
parsa <parsamesg@gmail.com>
2021-11-22 16:06:02 +0000

    post update route

--- routes/location.js
+++ routes/location.js
@@ -4,5 +4,6 @@ var router = express.Router();
 const LocationController = require("../controllers/location");
 
 router.get("/", LocationController.Show);
+router.post("/update", LocationController.Update);
 
 module.exports = router;
